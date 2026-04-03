b73e3244206da649ed23829092b4acf3ca8eb0f1
joressHACK24 <joresfotso01.gmail.com>
2026-04-03 18:46:24 +0200

    Histogramm-Generierung und Fehlerbehandlung für ungültige Dateien hinzugefügt

--- Projekt2-Statistikrechner/Statistikrechner.py
+++ Projekt2-Statistikrechner/Statistikrechner.py
@@ -2,18 +2,27 @@ import argparse
 import csv
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # nehme ich die Datei der Nutzer ein
 parser = argparse.ArgumentParser("Lesen der Datei von der Nutzer")
 parser.add_argument("csv_file", help="Pfad zur Datei")
 
-args = parser.parse_args()
+try:
+    args = parser.parse_args()
+except FileNotFoundError:
+    print("Ihre Datei ist nicht gefunden ")
+except NameError:
+    print( " Bitte geben sie ein csv-Datei")
 
 
 # muss ich jetzt eine Spalte auswählen und die Mittelwert, Varianz und median rechnen
 
 Daten = pd.read_csv(args.csv_file, sep=';')
+
+
+
 # On garde tout SAUF les colonnes de type 'object'
 Daten = Daten.select_dtypes(include=['number'])
 
@@ -24,6 +33,8 @@ for data in Daten.columns:
     Median  = np.median(Daten[data])
 
     plt.hist(Daten[data])
+    plt.savefig(f"hist_{data}")
     print(f"Da sind die statistische Komponent der Spalte '{data}':\n Mittelwert {Mittelwert},\n Varianz {Varianz},\n Mediane {Median}\n")
+
 
 
